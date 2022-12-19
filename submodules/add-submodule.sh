700be084eead324a2c9daf3ad506fb0e61b2a9e4
Richard Joubert <rjoubert@nsd.team>
2022-12-19 14:33:26 -0500

    ?
    Signed-off-by: Richard Joubert <rjoubert@nsd.team>

---
+++ submodules/add-submodule.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# ivoryAl@arm99
+
+git submodule add git@github.com:NewSpectrum/Better-Acode-Comments.git ./Better-Acode-Comments
+git submodule add git@github.com:NewSpectrum/Acode-SourceCode ./Acode-SourceCode
